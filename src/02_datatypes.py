1eb5d82014f33022e716b9ba3a9018cd3e36793c
Fatima Rizvi <fatimarzv23@gmail.com>
2020-11-23 10:17:43 -0600

    Completed hello world, big num, data types, and modules practices

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,20 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+# y = int(y)
+# print(y + x)
+print(x + int(y))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+# x = str(x)
+# y = str(y)
+
+print(str(x) + y)
+
+# print(str(x) + str(y))
+#print(x + y)
+
+# z = int(y)
+# print(x + z)
